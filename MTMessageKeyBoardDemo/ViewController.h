80df69693c4504170e3d4e9b7ee17ea2765a3200
dongxuwei <287178790@qq.com>
2016-12-21 10:52:42 +0800

    Initial Commit

---
+++ MTMessageKeyBoardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTMessageKeyBoardDemo
+//
+//  Created by 董徐维 on 2016/12/21.
+//  Copyright © 2016年 Mr.Tung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MTMessageKeyBoardDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MTMessageKeyBoardDemo
+//
+//  Created by 董徐维 on 2016/12/21.
+//  Copyright © 2016年 Mr.Tung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
